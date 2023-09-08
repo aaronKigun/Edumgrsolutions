669448d0f10c0025305ed7b4a35ee57ccd88b537
Aaron Kigun <aaronkigun@gmail.com>
2023-09-08 03:30:00 +0100

    Changes to be committed: 	new file:   Edumgrsolutions/.firebase/hosting.ZGlzdA.cache 	modified:   Edumgrsolutions/firebase.json 	new file:   Edumgrsolutions/public/assets/AES.png 	new file:   Edumgrsolutions/public/assets/BMMS.png 	new file:   Edumgrsolutions/public/assets/Black Faq.png 	new file:   Edumgrsolutions/public/assets/facebook.png 	new file:   Edumgrsolutions/public/assets/fatima.png 	new file:   Edumgrsolutions/public/assets/index.js 	new file:   Edumgrsolutions/public/assets/locate.png 	new file:   Edumgrsolutions/public/assets/sjcvom.png 	new file:   Edumgrsolutions/public/assets/ticktok.png 	new file:   Edumgrsolutions/public/assets/twitter.jpeg 	new file:   Edumgrsolutions/public/assets/website.png 	new file:   Edumgrsolutions/public/assets/whatsapp.png 	modified:   Edumgrsolutions/public/index.html 	modified:   Edumgrsolutions/vite.config.js

---
+++ Edumgrsolutions/public/assets/index.js
@@ -0,0 +1,25 @@
+import AES from "./AES.png"
+import BMMS from "./BMMS.png"
+import Fatima from "./fatima.png"
+import SJCVOM from "./sjcvom.png"
+import FAQ from "./Black Faq.png"
+import Locate from "./locate.png"
+import Website from "./website.png"
+import Facebook from "./facebook.png"
+import Twitter from "./twitter.jpeg"
+import Whatsapp from "./whatsapp.png"
+import Ticktok from "./ticktok.png"
+
+export {
+    AES,
+    BMMS,
+    Fatima,
+    SJCVOM,
+    FAQ,
+    Locate,
+    Website,
+    Facebook,
+    Twitter,
+    Whatsapp,
+    Ticktok,
+};
