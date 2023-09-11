6c1bf4da41c811cb915433c39c89779376f5ae86
Aaron Kigun <aaronkigun@gmail.com>
2023-09-11 12:19:22 +0100

    Changes to be committed: 	modified:   Edumgrsolutions/package-lock.json 	modified:   Edumgrsolutions/package.json 	modified:   Edumgrsolutions/public/index.html 	modified:   Edumgrsolutions/src/main.jsx

--- Edumgrsolutions/src/main.jsx
+++ Edumgrsolutions/src/main.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAz_bhVPLY1u-uCWGDR262KCq6bpIaxRSE",
+  authDomain: "edumgrsolutionss.firebaseapp.com",
+  projectId: "edumgrsolutionss",
+  storageBucket: "edumgrsolutionss.appspot.com",
+  messagingSenderId: "473387518200",
+  appId: "1:473387518200:web:fe63cc1da0b6378fecd30d",
+  measurementId: "G-0Q0DL82Q50"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
